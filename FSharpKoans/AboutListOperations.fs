52a7e630247ed0d886cf8e841cc49b0d1549fde6
Robin <gvgollie4@gmail.com>
2019-03-29 20:08:36 +0200

    Almost finished

--- FSharpKoans/AboutListOperations.fs
+++ FSharpKoans/AboutListOperations.fs
@@ -20,6 +20,7 @@ Run it with some sample input, and see what it produces.
 *)
 
 module ``14: List operations are so easy, you could make them yourself!`` =
+    open System.Xml
 
     (*
         Once you've learned about the Easy Way of doing something,
@@ -50,8 +51,8 @@ module ``14: List operations are so easy, you could make them yourself!`` =
         let rev (xs : 'a list) : 'a list =
            let rec len rest out = // write a function to reverse a list here.
             match rest with
-            | [] -> []
-            | _::rest -> len rest (rest::out)
+            | [] -> out
+            | x::rest -> len rest (x::out)
            len xs [] 
         rev [9;8;7] |> should equal [7;8;9]
         rev [] |> should equal []
@@ -69,7 +70,11 @@ module ``14: List operations are so easy, you could make them yourself!`` =
     [<Test>]
     let ``05 Fixed-function mapping, the hard way (part 1).`` () =
         let map (xs : int list) : int list =
-            __ // write a function which adds 1 to each element
+           let rec len lis out = // write a function which adds 1 to each element
+            match lis with
+            | [] -> List.rev(out)
+            | x::lis -> len lis (x+1 ::out)
+           len xs []  
         map [1; 2; 3; 4] |> should equal [2; 3; 4; 5]
         map [9; 8; 7; 6] |> should equal [10; 9; 8; 7]
         map [15; 2; 7] |> should equal [16; 3; 8]
@@ -79,7 +84,11 @@ module ``14: List operations are so easy, you could make them yourself!`` =
     [<Test>]
     let ``06 Fixed-function mapping, the hard way (part 2).`` () =
         let map (xs : int list) : int list =
-            __ // write a function which doubles each element
+           let rec len lis out = // write a function which doubles each element
+            match lis with
+            | [] -> List.rev(out)
+            | x::lis -> len lis (x*2 ::out)
+           len xs []   
         map [1; 2; 3; 4] |> should equal [2; 4; 6; 8]
         map [9; 8; 7; 6] |> should equal [18; 16; 14; 12]
         map [15; 2; 7] |> should equal [30; 4; 14]
@@ -98,7 +107,11 @@ module ``14: List operations are so easy, you could make them yourself!`` =
     [<Test>]
     let ``07 Specified-function mapping, the hard way`` () =
         let map (f : 'a -> 'b) (xs : 'a list) : 'b list =
-            __ // write a map which applies f to each element
+           let rec len lis out = // write a map which applies f to each element
+            match lis with
+            | [] -> List.rev(out)
+            | x::lis -> len lis (f x ::out)
+           len xs []   
         map (fun x -> x+1) [9;8;7] |> should equal [10;9;8]
         map ((*) 2) [9;8;7] |> should equal [18;16;14]
         map (fun x -> sprintf "%.2f wut?" x)  [9.3; 1.22] |> should equal ["9.30 wut?"; "1.22 wut?"]
@@ -113,7 +126,15 @@ module ``14: List operations are so easy, you could make them yourself!`` =
     [<Test>]
     let ``09 Specified-function filtering, the hard way`` () =
         let filter (f : 'a -> bool) (xs : 'a list) : 'a list =
-            __ // write a function which filters based on the specified criteria
+           let rec len lis out  = // write a function which filters based on the specified criteria
+            match lis with
+            | [] -> List.rev(out)
+            | x::lis -> 
+              match f x with 
+              |true -> len lis (x::out)
+              |_ -> len (lis) out
+           len xs []
+               
         filter (fun x -> x > 19) [9; 5; 23; 66; 4] |> should equal [23; 66]
         filter (fun x -> String.length x = 4) ["moo"; "woof"; "yip"; "nyan"; "meow"]
         |> should equal ["woof"; "nyan"; "meow"]
@@ -130,7 +151,14 @@ module ``14: List operations are so easy, you could make them yourself!`` =
     [<Test>]
     let ``11 Fixed-function filtering, the hard way`` () =
         let filter (xs : int list) : int list =
-            __ // write a function to filter for odd elements only.
+           let rec len lis out  = // write a function to filter for odd elements only.
+            match lis with
+            | [] -> List.rev(out)
+            | x::lis -> 
+              match x%2=0 with 
+              |false -> len lis (x::out)
+              |_ -> len (lis) out
+           len xs [] 
         filter [1; 2; 3; 4] |> should equal [1; 3]
         filter [10; 9; 8; 7] |> should equal [9; 7]
         filter [15; 2; 7] |> should equal [15; 7]
@@ -148,7 +176,14 @@ module ``14: List operations are so easy, you could make them yourself!`` =
     [<Test>]
     let ``12 Specified-function filtering, the hard way`` () =
         let filter (f : 'a -> bool) (xs : 'a list) : 'a list =
-            __ // write a function which filters based on the specified criteria
+           let rec len lis out  = // write a function which filters based on the specified criteria
+            match lis with
+            | [] -> List.rev(out)
+            | x::lis -> 
+              match f x with 
+              |true -> len lis (x::out)
+              |_ -> len (lis) out
+           len xs [] 
         filter (fun x -> x > 19) [9; 5; 23; 66; 4] |> should equal [23; 66]
         filter (fun x -> String.length x = 4) ["moo"; "woof"; "yip"; "nyan"; "meow"]
         |> should equal ["woof"; "nyan"; "meow"]
@@ -195,18 +230,26 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``14 A fold which sums a list`` () =
         let fold initialState xs =
-            __ // write a function to do what's described above
+           let rec len lis NS = // write a function to do what's described above
+            match lis with
+            | [] -> NS + initialState
+            | x::lis -> len lis (x + NS)
+           len xs 0  
         fold 0 [1; 2; 3; 4] |> should equal 10
         fold 100 [2;4;6;8] |> should equal 120
 
     [<Test>]
     let ``15 A fold which multiplies a list`` () =
-        let fold initialState xs =
-            __ // write a function to multiply the elements of a list
-        fold __ [99] |> should equal 99
-        fold 2 [__] |> should equal 22
-        fold __ [1;3;5;7] |> should equal 105
-        fold __ [2;5;3] |> should equal 0
+        let fold initialState xs = // write a function to multiply the elements of a list
+           let rec len lis NS = 
+            match lis with
+            | [] -> NS 
+            | x::lis -> len lis (x * NS)
+           len xs initialState  
+        fold 1 [99] |> should equal 99
+        fold 2 [11] |> should equal 22
+        fold 1 [1;3;5;7] |> should equal 105
+        fold 0 [2;5;3] |> should equal 0
 
     // you probably know the drill by now.  It'd be good to have
     // a function which does the state-generation stuff, wouldn't
@@ -215,7 +258,11 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``16 Folding, the hard way`` () =
         let fold (f : 'a -> 'b -> 'a) (initialState : 'a) (xs : 'b list) : 'a =
-            __  // write a function to do a fold.
+           let rec len lis d =  // write a function to do a fold.
+            match lis with
+            |[] -> f
+            |x::lis -> len ((f x)::lis)
+           len xs 
         fold (+) 0 [1;2;3;4] |> should equal 10
         fold (*) 2 [1;2;3;4] |> should equal 48
         fold (fun state item -> sprintf "%s %s" state item) "items:" ["dog"; "cat"; "bat"; "rat"]
@@ -235,7 +282,14 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``18 exists: finding whether any matching item exists`` () =
         let exists (f : 'a -> bool) (xs : 'a list) : bool =
-           __ // Does this: https://msdn.microsoft.com/en-us/library/ee370309.aspx
+           let rec len lis  = // Does this: https://msdn.microsoft.com/en-us/library/ee370309.aspx
+            match lis with
+            | [] -> false
+            | x::lis -> 
+              match f x with 
+              |true -> true
+              |_ -> len (lis)
+           len xs   
            
         exists ((=) 4) [7;6;5;4;5] |> should equal true
         exists (fun x -> String.length x < 4) ["true"; "false"] |> should equal false
@@ -245,7 +299,14 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``19 partition: splitting a list based on a criterion`` () =
         let partition (f : 'a -> bool) (xs : 'a list) : ('a list) * ('a list) =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee353782.aspx
+           let rec len lis (out1,out2) = //Does this: https://msdn.microsoft.com/en-us/library/ee353782.aspx
+            match lis with
+            | [] -> List.rev(out1), List.rev(out2)
+            | x::lis -> 
+              match f x with 
+              |true -> len lis ((x::out1),out2)
+              |_ -> len (lis) (out1, (x::out2))
+           len xs ([],[])    
         let a, b = partition (fun x -> x%2=0) [1;2;3;4;5;6;7;8;9;10]
         a |> should equal [2;4;6;8;10]
         b |> should equal [1;3;5;7;9]
@@ -268,7 +329,14 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``21 tryFind: find the first matching element, if any`` () =
         let tryFind (p : 'a -> bool) (xs : 'a list) : 'a option =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee353506.aspx
+          let rec len lis  = // Does this: https://msdn.microsoft.com/en-us/library/ee353506.aspx
+            match lis with
+            | [] -> None
+            | x::lis -> 
+              match p x with 
+              |true -> Some x
+              |_ -> len (lis) 
+          len xs      
         tryFind (fun x -> x<=45) [100;85;25;55;6] |> should equal (Some 25)
         tryFind (fun x -> x>450) [100;85;25;55;6] |> should equal None
 
@@ -276,7 +344,14 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``22 tryPick: find the first matching element, if any, and transform it`` () =
         let tryPick (p : 'a -> 'b option) (xs : 'a list) : 'b option =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee353814.aspx
+          let rec len lis  =  // Does this: https://msdn.microsoft.com/en-us/library/ee353814.aspx
+            match lis with
+            | [] -> None
+            | x::lis -> 
+              match p x with 
+              |Some x -> Some x
+              |_ -> len (lis) 
+          len xs       
         let f x =
             match x<=45 with
             | true -> Some(x*2)
@@ -305,8 +380,15 @@ or something else), it's likely that you'll be able to use a fold.
         // Think about this: why does the signature of `choose` have to be like this?
         // - why can't it take an 'a->'b, instead of an 'a->'b option ?
         // - why does it return a 'b list, and not a 'b list option ?
-        let choose (p : 'a -> 'b option) (xs : 'a list) : 'b list =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee353456.aspx
+        let choose (p : 'a -> 'b option) (xs : 'a list) : 'b list = 
+          (*let rec len lis out =  // Does this: https://msdn.microsoft.com/en-us/library/ee353456.aspx
+            match lis with
+            | [] -> None
+            | x::lis -> 
+              match p x with 
+              |Some x -> (out :: Some x)
+              |_ -> len (lis) []
+          len xs []*)         
         let f x =
             match x<=45 with
             | true -> Some(x*2)
@@ -324,7 +406,14 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``24 mapi: like map, but passes along an item index as well`` () =
         let mapi (f : int -> 'a -> 'b) (xs : 'a list) : 'b list =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee353425.aspx
+          let rec len f lis  =  // Does this: https://msdn.microsoft.com/en-us/library/ee353425.aspx
+            match lis with
+            | [] -> List.rev(out1), List.rev(out2)
+            | x::lis -> 
+              match f x with 
+              |true -> len lis ((x::out1),out2)
+              |_ -> len (lis) (out1, (x::out2))
+          len xs ([],[])     
         mapi (fun i x -> -i, x+1) [9;8;7;6] |> should equal [0,10; -1,9; -2,8; -3,7]
         let hailstone i t =
             match i%2 with
